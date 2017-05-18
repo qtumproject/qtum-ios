42504acd809d1b7285df80456efb123720f7787c
Sharaev Vladimir <sharaev.vladimir@gmail.com>
2017-05-18 14:40:15 +0300

    Changes for screen, completed storyboard

--- qtum wallet/AddNewTokensViewController.m
+++ qtum wallet/AddNewTokensViewController.m
@@ -8,10 +8,15 @@
 
 #import "AddNewTokensViewController.h"
 #import "SubscribeTokenCoordinator.h"
+#import "TextFieldWithLine.h"
 
-@interface AddNewTokensViewController ()
+const CGFloat TextFieldAlpha = 0.3f;
 
-@property (weak, nonatomic) IBOutlet UITextField *tokenAddressTextField;
+@interface AddNewTokensViewController () <UITextFieldDelegate, UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet TextFieldWithLine *tokenAddressTextField;
+@property (weak, nonatomic) IBOutlet UIView *lineView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 - (IBAction)acitonBack:(id)sender;
 - (IBAction)actionAddTokens:(id)sender;
@@ -22,7 +27,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+    
+    self.tokenAddressTextField.delegate = self;
+    self.scrollView.delegate = self;
 }
+
+#pragma mark - Notifications Handlers
+
+-(void)keyboardWillShow:(NSNotification *)sender{
+    CGRect end = [[sender userInfo][UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    UIEdgeInsets insets = UIEdgeInsetsMake(0.0f, 0.0f, end.size.height, 0.0f);
+    self.scrollView.contentInset = insets;
+}
+
+-(void)keyboardWillHide:(NSNotification *)sender{
+    self.scrollView.contentInset = UIEdgeInsetsZero;
+}
+
+#pragma mark - Actions
 
 - (IBAction)acitonBack:(id)sender
 {
@@ -32,6 +63,37 @@
 - (IBAction)actionAddTokens:(id)sender
 {
     
+}
+
+#pragma UITextFieldDelegate
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    self.lineView.alpha = 1.0f;
+    self.tokenAddressTextField.alpha = 1.0f;
+    
+    return YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    if ([textField.text isEqualToString: @""]) {
+        self.lineView.alpha = TextFieldAlpha;
+        self.tokenAddressTextField.alpha = TextFieldAlpha;
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+#pragma maek - UIScrollViewDelegate
+
+- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView
+{
+    [self.tokenAddressTextField resignFirstResponder];
 }
 
 @end
