0ae06c344b002e3bd9c9fee2ea8bcdeff211d1b8
Sharaev Vladimir <sharaev.vladimir@gmail.com>
2017-05-22 12:39:16 +0300

    Fixed header

--- qtum wallet/ActivityTokenTableViewCell.h
+++ qtum wallet/ActivityTokenTableViewCell.h
@@ -3,7 +3,7 @@
 //  qtum wallet
 //
 //  Created by Sharaev Vladimir on 19.05.17.
-//  Copyright © 2017 Designsters. All rights reserved.
+//  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
