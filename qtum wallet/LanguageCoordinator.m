d7c590811dbcd42600fd5998d5101752a6dc5086
Sharaev Vladimir <sharaev.vladimir@gmail.com>
2017-05-23 15:33:31 +0300

    Fixed headers in new files

--- qtum wallet/LanguageCoordinator.m
+++ qtum wallet/LanguageCoordinator.m
@@ -3,7 +3,7 @@
 //  qtum wallet
 //
 //  Created by Sharaev Vladimir on 23.05.17.
-//  Copyright © 2017 Designsters. All rights reserved.
+//  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
 #import "LanguageCoordinator.h"
