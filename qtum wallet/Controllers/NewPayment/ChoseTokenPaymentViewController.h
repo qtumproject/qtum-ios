dcdb79494288f253780d72afad34512cd15b49c8
Vladimir Lebedevich <lebedevich@yahoo.com>
2017-05-29 11:26:41 +0300

    rename commends

--- qtum wallet/Controllers/NewPayment/ChoseTokenPaymentViewController.h
+++ qtum wallet/Controllers/NewPayment/ChoseTokenPaymentViewController.h
@@ -2,7 +2,7 @@
 //  ChoseTokenPaymentViewController.h
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 25.05.17.
+//  Created by Vladimir Lebedevich on 25.05.17.
 //  Copyright © 2017 Designsters. All rights reserved.
 //
 
