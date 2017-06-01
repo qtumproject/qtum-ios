fc32a08e0bd36c0f10abfdcaa03027cc6bdadb01
Sharaev Vladimir <sharaev.vladimir@gmail.com>
2017-06-01 13:35:11 +0300

    Fixed header

--- qtum wallet/MainTokenTableViewCell.m
+++ qtum wallet/MainTokenTableViewCell.m
@@ -3,7 +3,7 @@
 //  qtum wallet
 //
 //  Created by Sharaev Vladimir on 29.05.17.
-//  Copyright © 2017 Designsters. All rights reserved.
+//  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
 #import "MainTokenTableViewCell.h"
