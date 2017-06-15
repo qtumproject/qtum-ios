3da62b15797d6f72ff7dacbf569ad8b5b44f3f52
Vladimir Lebedevich <lebedevich@yahoo.com>
2017-06-15 15:45:11 +0300

    versiona and comments fixe

--- qtum wallet/WatchTokensViewController.h
+++ qtum wallet/WatchTokensViewController.h
@@ -2,7 +2,7 @@
 //  WatchTokensViewController.h
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 12.06.17.
+//  Created by Vladimir Lebedevich on 12.06.17.
 //  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
