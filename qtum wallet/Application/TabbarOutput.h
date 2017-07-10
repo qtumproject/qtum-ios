d03d42ffed3b41193ee806f48b21a5ac582c2c7e
Vladimir Lebedevich <lebedevich@yahoo.com>
2017-07-10 14:14:31 +0300

    comments renamed

--- qtum wallet/Application/TabbarOutput.h
+++ qtum wallet/Application/TabbarOutput.h
@@ -2,7 +2,7 @@
 //  TabbarOutput.h
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 05.07.17.
+//  Created by Vladimir Lebedevich on 05.07.17.
 //  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
--- qtum wallet/Controllers/NewPayment/ChooseTokenPaymentDelegateDataSourceDark.h
+++ qtum wallet/Controllers/NewPayment/ChooseTokenPaymentDelegateDataSourceDark.h
@@ -2,7 +2,7 @@
 //  ChooseTokenPaymentDelegateDataSourceDark.h
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 10.07.17.
+//  Created by Vladimir Lebedevich on 10.07.17.
 //  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
