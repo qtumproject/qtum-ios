10bbbd4bc0771357c1da1c73bc6f72c45197d6c2
Vladimir Lebedevich <lebedevich@yahoo.com>
2017-07-12 11:18:40 +0300

    comments renamed

--- qtum wallet/ExportBrainKeyViewControllerDark.m
+++ qtum wallet/ExportBrainKeyViewControllerDark.m
@@ -2,7 +2,7 @@
 //  ExportBrainKeyViewControllerDark.m
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 12.07.17.
+//  Created by Vladimir Lebedevich on 12.07.17.
 //  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
