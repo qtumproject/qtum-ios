07ac09ddf84c8f7a231541f2a1cb95c2e6bfdbaa
Vladimir Lebedevich <lebedevich@yahoo.com>
2017-08-01 10:52:18 +0300

    comments renamed

--- qtum wallet/SecurityPopupViewControllerDark.h
+++ qtum wallet/SecurityPopupViewControllerDark.h
@@ -2,7 +2,7 @@
 //  SecurityPopupViewControllerDark.h
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 31.07.17.
+//  Created by Vladimir Lebedevich on 31.07.17.
 //  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
--- qtum wallet/TemplatesListOutputDelegate.h
+++ qtum wallet/TemplatesListOutputDelegate.h
@@ -2,7 +2,7 @@
 //  TemplatesListOutputDelegate.h
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 28.07.17.
+//  Created by Vladimir Lebedevich on 28.07.17.
 //  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
