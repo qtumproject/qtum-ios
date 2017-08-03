35961a861a4dc7052aba2c7ff7102ee0e9d2327c
Vladimir Lebedevich <lebedevich@yahoo.com>
2017-08-03 15:51:54 +0300

    comments renamed

--- qtum wallet/TokenAddressLibraryOutput.h
+++ qtum wallet/TokenAddressLibraryOutput.h
@@ -2,7 +2,7 @@
 //  TokenAddressLibraryOutput.h
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 03.08.17.
+//  Created by Vladimir Lebedevich on 03.08.17.
 //  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
