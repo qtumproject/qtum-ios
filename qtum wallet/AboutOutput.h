ddd79295fa2bd3cf01e45f2f614f64ebc126d852
Vladimir Lebedevich <lebedevich@yahoo.com>
2017-08-29 15:16:21 +0300

    files renaming

--- qtum wallet/AboutOutput.h
+++ qtum wallet/AboutOutput.h
@@ -2,7 +2,7 @@
 //  AboutOutput.h
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 28.08.17.
+//  Created by Vladimir Lebedevich on 28.08.17.
 //  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
--- qtum wallet/QStoreTemplateDetailViewControllerLight.m
+++ qtum wallet/QStoreTemplateDetailViewControllerLight.m
@@ -2,7 +2,7 @@
 //  QStoreTemplateDetailViewControllerLight.m
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 24.08.17.
+//  Created by Vladimir Lebedevich on 24.08.17.
 //  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
