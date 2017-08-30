23d93dec6285eb62fc96a7a201bfb6edb70fa8f9
Vladimir Lebedevich <lebedevich@yahoo.com>
2017-08-30 17:31:35 +0300

    comments renamed

--- qtum wallet/Model/AbiParameterTypeFixedArrayInt.h
+++ qtum wallet/Model/AbiParameterTypeFixedArrayInt.h
@@ -2,7 +2,7 @@
 //  AbiParameterTypeFixedArrayInt.h
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 30.08.17.
+//  Created by Vladimir Lebedevich on 30.08.17.
 //  Copyright © 2017 PixelPlex. All rights reserved.
 //
 
