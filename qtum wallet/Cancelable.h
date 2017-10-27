005bad04b35a33820b35907b9119a3e69f62bb8d
Vladimir Lebedevich <lebedevich@yahoo.com>
2017-10-27 16:11:02 +0300

    comments renamed

--- qtum wallet/Cancelable.h
+++ qtum wallet/Cancelable.h
@@ -2,7 +2,7 @@
 //  Cancelable.h
 //  qtum wallet
 //
-//  Created by Никита Федоренко on 27.10.17.
+//  Created by Vladimir Lebedevich on 27.10.17.
 //  Copyright © 2017 QTUM. All rights reserved.
 //
 
